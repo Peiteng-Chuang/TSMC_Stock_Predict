857b2049ada83ff0b1c7c07a04b6a0a5113afbc3
peiteng <peiteng.chuang@gmail.com>
2024-06-05 10:16:24 +0800

    105% acc I made a shit

---
+++ typeG/v2.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from tensorflow.keras.layers import LSTM,Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error
+
+MOVING_WIN_SIZE=35
+DS_SPLIT=0.8
+
+typeg_data=pd.read_csv("./typeG/data/2330.TW.csv")
+
+test_10day=typeg_data.rename(columns={'Close':'clo'})
+test_day=test_10day.filter(['clo'])
+test_day=test_day.reset_index(drop=True)
+
+from sklearn.preprocessing import MinMaxScaler
+import numpy as  np
+
+scaler=MinMaxScaler(feature_range=(0,1))
+scaler_prices=scaler.fit_transform(test_day.values)
+
+all_x,all_y=[],[]
+for i in range(len(scaler_prices)-MOVING_WIN_SIZE):
+    x=scaler_prices[i:i+MOVING_WIN_SIZE]
+    y=scaler_prices[i+MOVING_WIN_SIZE]
+    all_x.append(x)
+    all_y.append(y)
+
+all_x,all_y=np.array(all_x),np.array(all_y)
+train_ds_size=round(all_x.shape[0]*DS_SPLIT)
+train_x,train_y=all_x[:train_ds_size],all_y[:train_ds_size]
+test_x,test_y=all_x[train_ds_size:],all_y[train_ds_size:]
+
+model =Sequential()
+model.add(LSTM(units=128,return_sequences=True,input_shape=(train_x.shape[1],1)))
+model.add(LSTM(units=50,return_sequences=False))
+model.add(Dense(units=25))
+model.add(Dense(units=1))
+
+model.compile(optimizer="adam",loss="mean_squared_error")
+
+callback=EarlyStopping( monitor="val_loss",patience=100,restore_best_weights=True)
+model.fit(train_x,train_y,
+          validation_split=0.2,
+          callbacks=[callback],
+          epochs=1000
+          )
+
+preds=scaler.inverse_transform(model.predict(test_x))
+train_data=test_day[:train_ds_size+MOVING_WIN_SIZE]
+test_data=test_day[train_ds_size+MOVING_WIN_SIZE:]
+test_data=test_data.assign(Predict=preds)
+preds=scaler.inverse_transform(model.predict(test_x))
+test_data=test_data.assign(Shifted=test_data['clo'].shift(1))
+test_data.iat[0,-1]=train_data.iat[-1,-1]
+
+predict_rmse=mean_squared_error(test_data['clo'],test_data['Predict'],squared=False)
+predict_cvrmse=predict_rmse / test_data['clo'].mean()*100
+
+shifted_rmse=mean_squared_error(test_data['clo'],test_data['Shifted'],squared=False)
+shifted_cvrmse=shifted_rmse / test_data['clo'].mean()*100
+
+
+print(f"predict\t=  {predict_cvrmse} % \nshift\t=  {shifted_cvrmse} %")
